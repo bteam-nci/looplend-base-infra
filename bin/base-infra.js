9ccd499513a0320f5172c3662782f3f747824f60
fborrelli <fborrelli@shutterstock.com>
2023-05-29 20:05:24 +0100

    fix: apply cdn

--- bin/base-infra.js
+++ bin/base-infra.js
@@ -4,4 +4,4 @@ import cdk from 'aws-cdk-lib'
 import BaseInfraStack from '../lib/base-infra-stack.js'
 
 const app = new cdk.App()
-new BaseInfraStack(app, `${stage}-looplend-base`)
+new BaseInfraStack(app, `looplend-base`)
