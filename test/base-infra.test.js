8d115a3530fbf9c9ad23b2df660dca60b96755e9
fborrelli <fborrelli@shutterstock.com>
2023-05-29 18:38:40 +0100

    Initial commit

---
+++ test/base-infra.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const BaseInfra = require('../lib/base-infra-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/base-infra-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new BaseInfra.BaseInfraStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
