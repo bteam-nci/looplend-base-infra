52e3bdf81433b8392bc1f6e7fa0a25928f42c9ff
SirFrancis <borrellifrn@gmail.com>
2023-07-01 13:10:44 +0100

    fix

--- bin/base-infra.js
+++ bin/base-infra.js
@@ -4,8 +4,8 @@ import cdk from 'aws-cdk-lib'
 import BaseInfraStack from '../lib/base-infra-stack.js'
 
 const app = new cdk.App()
-const env  = { account: '925477059004', region: 'eu-west-1' };
+const env = { account: '925477059004', region: 'eu-west-1' }
 
 new BaseInfraStack(app, `looplend-base`, {
-    env
+    env,
 })
